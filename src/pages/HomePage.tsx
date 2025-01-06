e95bd9bd3099b2d7223a611509a516576f3752f1
Patryk Jurak <patryk52310@gmail.com>
2025-01-06 12:11:36 +0100

    dodano prototyp MainPage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,16 @@
+import { Link } from "react-router-dom"
+import Lottie from "lottie-react"
+import chefAnimation from "../assets/chefAnimation.json"
+import "../styles/MainPage.css"
+
 export const HomePage = () => {
     return (
-        <h1>Sigma u</h1>
+        <div className="main-page">
+            <div>
+                <h1 className="title">Książka Kucharska</h1>
+                <Link to="/recipes" className="link">Przepisy</Link>
+            </div>
+            <Lottie className="animation" animationData={chefAnimation} loop autoplay allowTransparency />
+        </div>
     )
 }
