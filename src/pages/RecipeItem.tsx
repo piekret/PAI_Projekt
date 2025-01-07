4a54c33ca3055061c9c1def0114e1d6ffb299ea5
Olus <dabrowskiolek532@gmail.com>
2025-01-07 15:24:07 +0100

    rozpoczeto prace nad RecipeItem

--- src/pages/RecipeItem.tsx
+++ src/pages/RecipeItem.tsx
@@ -1,3 +1,26 @@
-export const RecipeList = () => {
-    
+import {useEffect, useState} from "react";
+import {useParams} from "react-router-dom";
+
+export const RecipeItem = () => {
+    const {id} = useParams();
+    const [recipe, setRecipe] = useState<Record<string, string | null>>();
+
+    useEffect(() => {
+        const getRecipe = async () => {
+            try {
+                const res = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
+                const data = await res.json();
+                setRecipe(data.meals[0]);
+            } catch(err) {
+                console.error("Wystąpił błąd: ", err);
+            }
+        }
+        getRecipe();
+    }, []);
+
+    return (
+        <>
+            <h1>{recipe.strMeal}</h1>
+        </>
+    )
 }
