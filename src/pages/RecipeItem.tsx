15782128d6389fbacf48f8bcd42e5c3283ebb885
Patryk Jurak <patryk52310@gmail.com>
2025-01-20 00:12:36 +0100

    dodano tło w RecipeItem bo zapomnialem wcześniej

--- src/pages/RecipeItem.tsx
+++ src/pages/RecipeItem.tsx
@@ -59,7 +59,7 @@ export const RecipeItem = () => {
     const ingredients = getIngredients();
 
     return (
-        <div className="bg-[url('https://www.interregeurope.eu/sites/default/files/news/Wood.jpg')] bg-cover bg-center bg-no-repeat">
+        <div className="bg-[url('https://www.interregeurope.eu/sites/default/files/news/Wood.jpg')] bg-cover bg-center bg-no-repeat min-h-screen">
                 <div 
                 className="mx-auto max-w-3xl p-8 bg-white rounded-lg shadow-md text-center"
                 style={{
